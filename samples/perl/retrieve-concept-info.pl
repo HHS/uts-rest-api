900f7409968d3d52f47ae77306e3a69a582b9b2a
Steve Emrick <steve.emrick@nih.gov>
2015-07-29 17:38:30 -0400

    Adding search-terms.pl Add Getopt::Std to allow users to enter username/pw info as parameters

--- samples/perl/retrieve-concept-info.pl
+++ samples/perl/retrieve-concept-info.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/perl
+
+##usage: perl retrieve-concept-info.pl -u your-umls-username -p your-umls-password
+##This file runs some searches on a list of UMLS CUIs and then prints out some basic information.
+##The full list of fields available for CUI retrieval is at https://documentation.uts.nlm.nih.gov/rest/search/index.html#sample-output
+
 use lib ".";
 use strict;
 use warnings;
@@ -7,20 +12,25 @@ use Authentication::TicketClient;
 use JSON;
 use REST::Client;
 use Data::Dumper;
-
+use Getopt::Std;
+our ($opt_u,$opt_p);
+getopt('up');
+my $username = $opt_u || die "please provide username";
+my $password = $opt_p || die "please provide password";
 ##create a ticket granting ticket for the session
-my $ticketClient = new TicketClient(username=>"your-umls-username",password=>"your-umls-password",service=>"http://umlsks.nlm.nih.gov",tgt=>"") || die "could not create TicketClient() object";
+my $ticketClient = new TicketClient(username=>$opt_u,password=>$opt_p,service=>"http://umlsks.nlm.nih.gov",tgt=>"") || die "could not create TicketClient() object";
 my $tgt = $ticketClient->getTgt();
 my $uri = new URI("https://uts-ws.nlm.nih.gov");
-my %params = ();
+my %parameters = ();
 my $client = REST::Client->new();
+
 open CUIS, "sample-cuis.txt" || die "Could not open sample file$!";
 
   while(<CUIS>) {
   	my $cui = $_;
   	chomp($cui);  	
   	my $path = "/rest/content/current/CUI/".$cui;
-  	my $json = run_query($path,%params);
+  	my $json = run_query($path,\%parameters);
     my $ui = $json->{result}{ui};
     my $name = $json->{result}{name} ;
     my @stys = @{ $json->{result}{semanticTypes} };
@@ -43,13 +53,12 @@ sub format_json {
 
 sub run_query {
 	
-	#TODO - add in handling for 404s once they are returned properly.
-	my ($path, %params) = @_;
-	$params{ticket} = $ticketClient->getServiceTicket();
+	my ($path, $parameters) = @_;	
+	$parameters{ticket} = $ticketClient->getServiceTicket();
 	$uri->path($path);
-	$uri->query_form(\%params);
+	$uri->query_form($parameters);
 	#print qq{$uri\n};
-	my $query = $client->GET($uri);
+	my $query = $client->GET($uri) || die "could not execute query";
 	my $results = $query->responseCode() eq '200'? $query->responseContent: "Not Found";
 	my $json = format_json($results) unless $results eq "Not Found";
 	return $json unless $results eq "Not Found";
---
+++ samples/perl/search-terms.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/perl
+
+##usage: perl search-terms.pl -u your-umls-username -p your-umls-password
+##This file runs some searches on a list of UMLS CUIs and then prints out some basic information.
+##The full list of fields available for search results is at https://documentation.uts.nlm.nih.gov/rest/concept/index.html#sample-output
+
+use lib ".";
+use strict;
+use warnings;
+use URI;
+use Authentication::TicketClient;
+use JSON;
+use REST::Client;
+use Data::Dumper;
+use Getopt::Std;
+our ($opt_u,$opt_p);
+getopt('up');
+my $username = $opt_u || die "please provide username";
+my $password = $opt_p || die "please provide password";
+
+##create a ticket granting ticket for the session
+my $ticketClient = new TicketClient(username=>$opt_u,password=>$opt_p,service=>"http://umlsks.nlm.nih.gov",tgt=>"") || die "could not create TicketClient() object";
+my $tgt = $ticketClient->getTgt();
+my $uri = new URI("https://uts-ws.nlm.nih.gov");
+my $json;
+my $client = REST::Client->new();
+my %parameters = ();
+open TERMS, "sample-terms.txt" || die "Could not open sample file$!";
+
+  while(<TERMS>) {
+  	my $term = $_;
+  	chomp($term);
+  	my $pageNum = "1";
+  	my $path = "/rest/search/current";
+  	
+  	$parameters{string} = $term;
+  	##optional parameters to return source-asserted identifiers and filter by source.  You can also use 'sourceConcept' (for SNOMEDCT_US, LNC, NCI,RXNORM) or 'sourceDescriptor' (for MSH,MDR,ICD9CM,ICD10CM,GO)
+  	#$parameters{returnIdType} = "code";
+  	#$parameters{sabs} = "SNOMEDCT_US,ICD10CM";
+  	
+  	print qq{Searching term $term\n};
+  	
+  	do {
+  		#many term searches will return more than the default page size, which is 25 json objects.
+  		$parameters{page} = $pageNum;
+  		$json = run_query($path,\%parameters);
+  		foreach my $result(@{ $json->{result}{results} }) {
+  		print qq{ $result->{ui}|$result->{uri}|$result->{rootSource}|$result->{name}\n } if $result->{name} ne 'NO RESULTS';
+  			
+  		}
+  		$pageNum++;
+
+  	}
+  	
+  	##TODO: Add 'next' and 'previous' page URIs to JSON output so we don't have to do this:.
+  	while $json->{result}{results}[0]{name} ne 'NO RESULTS';
+  	
+  }
+  
+  
+sub format_json {
+	my $json_in = shift;
+	my $json = JSON->new;
+	my $obj = $json->decode($json_in);
+	#print Dumper(\$obj);
+	return $obj;
+}
+
+sub run_query {
+	
+	my ($path, $parameters) = @_;	
+	$parameters{ticket} = $ticketClient->getServiceTicket();
+	$uri->path($path);
+	$uri->query_form($parameters);
+	#print qq{$uri\n};
+	my $query = $client->GET($uri) || die "could not execute query";
+	my $results = $query->responseCode() eq '200'? $query->responseContent: "Not Found";
+	my $json = format_json($results) unless $results eq "Not Found";
+	return $json unless $results eq "Not Found";
+}
+
