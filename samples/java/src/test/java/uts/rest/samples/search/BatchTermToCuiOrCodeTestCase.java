427a580e3d7f580fcbef810c0b7252607edd4c18
Emrick, Steve (NIH/NLM) [E] <emricks@mail.nih.gov>
2016-02-23 11:40:33 -0500

    Add semantic type information when resolving terms to CUIs.

--- samples/java/src/test/java/uts/rest/samples/search/BatchTermToCuiOrCodeTestCase.java
+++ samples/java/src/test/java/uts/rest/samples/search/BatchTermToCuiOrCodeTestCase.java
@@ -1,5 +1,7 @@
 /*This example loads a file of disease names (findings.txt from the 'resources' folder)
  * and retrieves the relevant UMLS CUI(s) or source-asserted codes
+ * Use Java VM args as follows:
+ * -Dusername=<username> -Dpassword=<password>
  */
 package uts.rest.samples.search;
 import java.io.BufferedReader;
@@ -8,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import uts.rest.samples.util.RestTicketClient;
 import uts.rest.samples.classes.SearchResult;
+import uts.rest.samples.classes.ConceptLite;
 import org.junit.Test;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
@@ -15,7 +18,7 @@ import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
 import static com.jayway.restassured.RestAssured.given;
-
+import static org.apache.commons.lang.StringUtils.join;
 
 public class BatchTermToCuiOrCodeTestCase {
 
@@ -41,13 +44,23 @@ public class BatchTermToCuiOrCodeTestCase {
 	       for(SearchResult result:results) {
 	    	
 	    	String cui = result.getUri();
+	    	String ui = result.getUi();
 	    	String name = result.getName();
 	    	String rsab = result.getRootSource();
+	    	/*TODO: Add info to the search results object so users 
+	    	 * know if the search object retrieved contains CUIs or source-asserted UIs.
+	    	 * For source-asserted UIs you would not be able to execute the code below which
+	    	 * retrieves the semantic type(s) for a CUI.
+	    	 */
+	    	ConceptLite concept = getConceptInfo(cui);
+	    	String stys[] = concept.getSemanticTypes();
 	    	
-	    	System.out.println("Result "+ num);
+	    	System.out.println("\nResult "+ num);
 	    	System.out.println("cui:"+ cui);
+	    	System.out.println("ui: " + ui);
 	    	System.out.println("name:"+ name);
 	    	System.out.println("Highest ranking source of cui:" + rsab);
+	    	System.out.println("Semantic Type(s): "+ join(stys,","));
 	    	num++;
 	       }
 	       System.out.println("----------");
@@ -74,7 +87,7 @@ public class BatchTermToCuiOrCodeTestCase {
 	                	//uncomment below to return only CUIs that have at least one non-obsolete/non-suppressible atom (relevant to the searchType) from the US Edition of SNOMED CT
 	                	//.param("sabs","SNOMEDCT_US")
 	                	//uncomment below to return CUIs that have at least one non-obsolete/non-suppressible atom that is an exact match with the search term
-	                    //.param("searchType","exact") //valid values are exact,words, approximate,leftTruncation,rightTruncation, and normalizedString
+	                    .param("searchType","exact") //valid values are exact,words, approximate,leftTruncation,rightTruncation, and normalizedString
 	                	//uncomment below to return source-asserted identifiers (from SNOMEDCT and other UMLS vocabularies) instead of CUIs
 	                	//.param("returnIdType","code")
 	        	 .expect()
@@ -95,6 +108,21 @@ public class BatchTermToCuiOrCodeTestCase {
 		}while(!results[0].getUi().equals("NONE"));		
 		return holder;
 		
+	}
+	
+	public ConceptLite getConceptInfo(String cui) {
+		Response response =  given()//.log().all()
+				.request().with()
+				.param("ticket", ticketClient.getST(tgt))
+				.expect()
+				.statusCode(200)
+				.when().get(cui);
+		
+		String output = response.getBody().asString();
+		Configuration config = Configuration.builder().mappingProvider(new JacksonMappingProvider()).build();
+		ConceptLite concept = JsonPath.using(config).parse(output).read("$.result",ConceptLite.class);
+        return concept;
+				
 	}
 }
 
