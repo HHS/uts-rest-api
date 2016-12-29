2f0fd30ee7bf1bce872e58dd1c190800437bb7ea
Steven P. Emrick <SPE@USP.org>
2016-12-29 15:24:59 -0500

    12/29/2016 - update with better algorithm on retrieval of ingredients

--- samples/python/get-rxcui-ingredients.py
+++ samples/python/get-rxcui-ingredients.py
@@ -2,10 +2,9 @@
 #coding=utf-8
 #get status of each rxcui
 #if active, get all ingredients (IN,MIN,PIN)
-#if there is a MIN, add that to the MIN dictionary, ignore others
-#if there is a PIN, add that to the PIN dictionary, ignore others
-#otherwise, add the single ingredient to the INS dictionary
-#todo - improve algorithm so that one call can be made to tty=IN+MIN+PIN, which requires an OrderedDict to make sure that MINS come first, then PINS, then INS
+#if there is a MIN, add that to the ingredients dictionary, ignore others
+#if there is a PIN, add that to the ingredients dictionary, ignore others
+#otherwise, add the single ingredient to the ingredients dictionary
 
 import requests
 import json
@@ -13,7 +12,6 @@ import argparse
 import simplejson
 import collections
 import sys
-from urllib import unquote
 from requests import utils
 parser = argparse.ArgumentParser(description='process user given parameters')
 parser.add_argument("-i", "--inputfile", required = True, dest = "inputfile", help = "enter a name for your input file")
@@ -25,10 +23,8 @@ args = parser.parse_args()
 inputfile=args.inputfile
 outputfile=args.outputfile
 
-INS = {}
-PINS = {}
-MINS = {}
-isPresent = False
+ingredients = {}
+complete = False
 
 def get(path,query):
   r = requests.get(uri+path, params=query)
@@ -41,61 +37,62 @@ def getRxCuiStatus(rxcui):
   results = get(path,query)
   return results
   
-def checkIngredient(rxcui,ingredient):
-   path = "/REST/rxcui/"+rxcui+"/related.json?tty="+ingredient
+def checkIngredient(rxcui):
+   path = "/REST/rxcui/"+rxcui+"/related.json?tty=IN+MIN+PIN"
    query = {}
    results=get(path,query)
    return results
    
 def parseIngredient(related):
-    global isPresent
+    
+    results = {}
+    global complete
+    complete = False
     
     for group in related["relatedGroup"]["conceptGroup"]:
         try:
            for properties in group["conceptProperties"]:
                #print properties["rxcui"]+", "+ properties["name"]
-               if group["tty"] == "MIN" and not properties["rxcui"] in MINS and isPresent == False:
-                  MINS[properties["rxcui"]] = properties["name"]
-                  isPresent = True
-                         
-               if group["tty"] == "PIN" and not properties["rxcui"] in PINS and isPresent == False:
-                  PINS[properties["rxcui"]] = properties["name"]
-                  isPresent = True
-               
-               if group["tty"] == "IN" and not properties["rxcui"] in INS and isPresent == False:
-                  INS[properties["rxcui"]] = properties["name"]
-                  isPresent = True
+               results[group["tty"]] = {"rxcui":properties["rxcui"],"tty":properties["tty"],"name":properties["name"]}
                   
         except:
            KeyError
-    return isPresent
-
+           
+   
+    for tty in sorted(results.keys()):
+        #print tty
+       if "MIN" in results and complete == False:
+           #print results["MIN"]
+           if results["MIN"]["rxcui"] not in ingredients.keys():
+              ingredients[results["MIN"]["rxcui"]] = results["MIN"]
+           complete = True
+       elif "MIN" not in results and "PIN" in results and complete == False:
+           #print results["PIN"]
+           if results["PIN"]["rxcui"] not in ingredients.keys():
+              ingredients[results["PIN"]["rxcui"]] = results["PIN"]
+           complete = True
+       elif "MIN" not in results and "PIN" not in results and complete == False:
+           #print results["IN"]
+           if results["IN"]["rxcui"] not in ingredients.keys():
+              ingredients[results["IN"]["rxcui"]] = results["IN"]
+           complete == True
+    
 with open(inputfile, 'r') as f:
-     for line in f:
-        isPresent = False
-        allIngredients = ("MIN","PIN","IN")
+     for line in f:        
         rxcui = line.strip()
         json = getRxCuiStatus(rxcui)
         status = json["rxcuiStatus"]["status"].encode('utf-8')
         
         if status == "Active":
-            for ingredient in allIngredients:
-                  if isPresent == False:
-                     related = checkIngredient(rxcui,ingredient)
-                     print "checking "+ingredient+ " for " + rxcui
-                     parseIngredient(related)
+            related = checkIngredient(rxcui)
+            parseIngredient(related)
                   
 
 f.close()
+
 w = open(outputfile, 'w')
 
-print "printing output file"
-            
-for rxcui,string in MINS.iteritems():
-   w.write(rxcui+"\tMIN\t"+string+"\n")
-
-for rxcui,string in PINS.iteritems():
-   w.write(rxcui+"\tPIN\t"+string+"\n")
-
-for rxcui,string in INS.iteritems():
-   w.write(rxcui+"\tIN\t"+string+"\n")
+for rows in sorted(ingredients.values()):
+    line = '\t'.join(rows.values())
+    w.write(line+"\n")
+    
